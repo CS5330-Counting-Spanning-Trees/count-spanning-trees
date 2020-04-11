5cadc51c8ed5fc7fa6e3bdf52ec1d82ce300ea09
pangjiahao <pangjiahao95@gmail.com>
2020-04-12 02:08:38 +0800

    added more testing functionality and storage functionality

--- termwise_approx.py
+++ termwise_approx.py
@@ -1,49 +1,33 @@
 # Experiments for approximating the term NST(G_i) / NST(G_{i-1})
-import pprint
-import copy
-import random
-from graphs import make_random_graph
+import pprint, copy, random, os
+import db, graphs
 from mtt import MTT
 from st_sampler import STSampler
 
-def printGraph(g):
-    pp = pprint.PrettyPrinter()
-    pp.pprint(g)
+subdir = 'testsuite1'
+filename = 'g_100_70.json'
+path = os.path.join(subdir, filename)
+data = db.load_data(path) # has the form [g1, NST(g1), g2, NST(g2), (u, v)]
 
-def num_edges(g): # assumes undirected
-    total = 0
-    for k, v in g.items():
-        total += len(v)
-    return total // 2
+g1 = db.fix_keys(data[0])
+nst1 = data[1]
+g2 = db.fix_keys(data[2])
+nst2 = data[3]
+e = data[4]
 
-def randomEdge(g): # chooses a random edge from g
-    random_u = random.choice(list(g.keys()))
-    random_v = random.choice(g[random_u]) # g[u] is not empty, assuming g is connected
-    return (random_u, random_v)
-
-g1 = make_random_graph(15, 0.5)
-NST1 = MTT(g1)
-g2 = copy.deepcopy(g1)
-u, v = randomEdge(g2)
-g2[u].remove(v)
-NST2 = MTT(g2)
-r = NST1 / NST2
+r = nst1 / nst2
 
 sampler = STSampler(g1)
 num_samples = 1000
 good = 0
 for i in range(num_samples):
-    print(i)
     t = sampler.sample()
-    if not (v in t[u]): # doesnt contains bad edge
+    if not (e[1] in t[e[0]]): # doesnt contains bad edge
         good += 1
 r_est = num_samples / good
 
-printGraph(g1)
-printGraph(g2)
+# graphs.printGraph(g1)
+# graphs.printGraph(g2)
 
 print('actual = {}; estimated = {}, ratio = {}'.format(r, r_est, r / r_est))
-
-printGraph(g1)
-printGraph(g2)
 
