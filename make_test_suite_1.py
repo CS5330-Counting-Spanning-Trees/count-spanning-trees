81dd3a097fc4e5c9fd04e6c6384f42491cc10515
pangjiahao <pangjiahao95@gmail.com>
2020-04-15 16:31:08 +0800

    Add some testing and graphing functionalities

--- make_test_suite_1.py
+++ make_test_suite_1.py
@@ -13,9 +13,16 @@ def get_random_edge(g):
     random_v = random.choice(g[random_u]) # g[u] is not empty, assuming g is connected
     return (random_u, random_v)
 
-num_vertices_list = [20, 50, 100, 120]
-densities_list = [0.1, 0.3, 0.5, 0.7]
-subdir = 'testsuite1'
+# subdir = 'testsuite2'
+# num_vertices_list = [20, 40, 60, 80, 100, 120]
+# densities_list = [0.1, 0.3, 0.5, 0.7, 0.9]
+# graph_number_list = list(range(1, 11))
+
+subdir = 'testsuite3'
+sample_sizes = [10, 50, 100, 200]
+num_vertices_list = [40, 60, 80]
+densities_list = [0.3, 0.5, 0.7]
+graph_number_list = list(range(1, 6))
 
 try:
     os.mkdir(subdir)
@@ -25,19 +32,20 @@ except FileExistsError:
 
 for n in num_vertices_list:
     for p in densities_list:
-        filename = 'g_{}_{}.json'.format(n, int(100*p))
-        path = os.path.join(subdir, filename)
+        for i in graph_number_list: # i gonna make 10 graphs of the same type (num_vertices and density)
+            filename = 'g{}_{}_{}.json'.format(i, n, int(100*p))
+            path = os.path.join(subdir, filename)
 
-        g1 = graphs.get_random_connected_graph(n, p)
-        u, v = get_random_edge(g1)
+            g1 = graphs.get_random_connected_graph(n, p)
+            u, v = get_random_edge(g1)
         
-        g2 = copy.deepcopy(g1)
-        g2[u].remove(v) # remove (u,v) from g2
-        g2[v].remove(u)
+            g2 = copy.deepcopy(g1)
+            g2[u].remove(v) # remove (u,v) from g2
+            g2[v].remove(u)
 
-        NST1 = mtt.MTT(g1)
-        NST2 = mtt.MTT(g2)
+            NST1 = mtt.MTT(g1)
+            NST2 = mtt.MTT(g2)
 
-        data = [g1, NST1, g2, NST2, [u,v]]
+            data = [g1, NST1, g2, NST2, [u,v]]
 
-        db.save_data(data, path)
+            db.save_data(data, path)
--- termwise_approx.py
+++ termwise_approx.py
@@ -1,33 +1,76 @@
 # Experiments for approximating the term NST(G_i) / NST(G_{i-1})
 import pprint, copy, random, os
-import db, graphs
+import db, graphs, plotting
+import numpy as np
 from mtt import MTT
 from st_sampler import STSampler
+from collections import defaultdict
 
-subdir = 'testsuite1'
-filename = 'g_100_70.json'
-path = os.path.join(subdir, filename)
-data = db.load_data(path) # has the form [g1, NST(g1), g2, NST(g2), (u, v)]
+# this is the main testing function
+# g = graph
+# edge = the edge removed to get g_minus_one_edge
+# actual ratio = NST(g) / NST(g_minus_one_edge)
+# num_samples = number of samples we will draw to calculate the estimate
+def get_error(g, g_minus_one_edge, edge, actual_ratio, num_samples):
+    sampler = STSampler(g)
+    good = 0 # number of samples without the 'bad' edge
+    for i in range(num_samples):
+        t = sampler.sample()
+        if not (edge[1] in t[edge[0]]): # doesnt contains bad edge
+            good += 1
+    r_est = num_samples / good
+    mult_error = abs(r_est - r) / r
+    return mult_error
 
-g1 = db.fix_keys(data[0])
-nst1 = data[1]
-g2 = db.fix_keys(data[2])
-nst2 = data[3]
-e = data[4]
+# we gonna test for all number of vertices, density, and sample_sizes
+# test_dir = 'testsuite2'
+# sample_sizes = [10, 50, 100, 200]
+# num_vertices_list = [20, 40, 60, 80, 100, 120]
+# densities_list = [0.1, 0.3, 0.5, 0.7, 0.9]
+# graph_number_list = list(range(1, 11))
 
-r = nst1 / nst2
+test_dir = 'testsuite3'
+sample_sizes = [10, 50, 100, 200]
+num_vertices_list = [40, 60, 80]
+densities_list = [0.3, 0.5, 0.7]
+graph_number_list = list(range(1, 6))
 
-sampler = STSampler(g1)
-num_samples = 1000
-good = 0
-for i in range(num_samples):
-    t = sampler.sample()
-    if not (e[1] in t[e[0]]): # doesnt contains bad edge
-        good += 1
-r_est = num_samples / good
+# where we will save the plotted data
+save_dir = 'pics'
+save_filename = 'termwise3.png'
+save_path = (os.path.join(save_dir, save_filename))
 
-# graphs.printGraph(g1)
-# graphs.printGraph(g2)
+plotdata = defaultdict(list)
+for n in num_vertices_list:
+    for density in densities_list:
+        mult_error_table = np.zeros((len(graph_number_list), len(sample_sizes)))
+        for gn in graph_number_list:
+            filename = 'g{}_{}_{}.json'.format(gn, n, int(100 * density))
+            path = (os.path.join(test_dir, filename))
+        
+            data = db.load_data(path) # has the form [g1, NST(g1), g2, NST(g2), (u, v)]
 
-print('actual = {}; estimated = {}, ratio = {}'.format(r, r_est, r / r_est))
+            g1 = db.fix_keys(data[0])
+            nst1 = data[1]
+            g2 = db.fix_keys(data[2])
+            nst2 = data[3]
+            e = data[4]
+
+            r = nst1 / nst2
+            for i in range(len(sample_sizes)):
+                mult_error = get_error(g1, g2, e, r, sample_sizes[i]) * 100 # in percent
+                mult_error_table[gn-1][i] = mult_error
+            
+        # take average along cols of mult_error_table
+        # covert to list and add to plot data
+        errors = list(np.mean(mult_error_table, axis=0))
+        dataset = list(zip(sample_sizes, errors))
+        plotdata[str((n, density))] = dataset
+
+        
+print(plotdata)
+plotting.plot_data_and_save(plotdata, save_path)
+
+
+
 
